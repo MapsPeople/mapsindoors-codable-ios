d314a9fd828808916528a833509b2d5957b61f8e
Christian Wolf Johannsen <chwo@mapspeople.com>
2025-10-13 15:50:50 +0200

    Release version 4.15.2-rc.3

--- Package.swift
+++ Package.swift
@@ -3,23 +3,28 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.15.2-rc.2")
+let mapsindoorsVersion = Version("4.15.2-rc.3")
+let checksum = "75e07bca1036bb22e1208453248fcb1093b54421b6a80fbceba2e8039728b9f6"
 
 let package = Package(
     name: "MapsIndoorsCodable",
     platforms: [.iOS(.v15)],
     products: [
-        .library(
-            name: "MapsIndoorsCodable",
-            targets: ["MapsIndoorsCodable"]),
+        .library(name: "MapsIndoorsCodable", targets: ["MapsIndoorsCodableWrapper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/MapsPeople/mapsindoors-core-ios.git", exact: mapsindoorsVersion),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "MapsIndoorsCodable",
+            url: "https://github.com/MapsPeople/MapsIndoors-SDK-iOS/releases/download/\(mapsindoorsVersion)/MapsIndoorsCodable.xcframework.zip",
+            checksum: checksum
+        ),
+        .target(
+            name: "MapsIndoorsCodableWrapper",
             dependencies: [
+                "MapsIndoorsCodable",
                 .product(name: "MapsIndoorsCore", package: "mapsindoors-core-ios"),
             ]
         ),
