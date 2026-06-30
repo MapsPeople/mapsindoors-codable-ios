090940838e4388137a0612e3317ec4369fa10f67
Christian Wolf Johannsen <chwo@mapspeople.com>
2026-06-30 17:38:33 +0200

    Release version 4.18.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let mapsindoorsVersion = Version("4.18.0-rc.1")
-let checksum = "7336e0731bba1ccd92243a4d3c2a2d0e7ce008b2f3d630160afb331a74eb16bc"
+let mapsindoorsVersion = Version("4.18.0")
+let checksum = "55a5fe1f43fd35389e095841b9d6b48b396b0f3162c2268eb9d083461688126f"
 
 let package = Package(
     name: "MapsIndoorsCodable",
